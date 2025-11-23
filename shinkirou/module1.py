7ffa9eafb68d72b7cb855b7d4ab6cd3bf028b914
Koukaze <45754998+koukaze@users.noreply.github.com>
2025-11-23 15:29:15 +0000

    refactor: add docstrings to module1 functions and update package version and description

--- shinkirou/module1.py
+++ shinkirou/module1.py
@@ -1,5 +1,11 @@
 def print_name():
+    """
+    Print the name of user.
+    """
     print("Koukaze")
 
 def age():
+    """
+    Print the age of user.
+    """
     print("18")
--- shinkirou/version.py
+++ shinkirou/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.post0'
-__version_tuple__ = version_tuple = (0, 1, 1, 'post0')
+__version__ = version = '0.1.1.post2'
+__version_tuple__ = version_tuple = (0, 1, 1, 'post2')
 
-__commit_id__ = commit_id = 'gd49733c55'
+__commit_id__ = commit_id = 'g594652f0d'
